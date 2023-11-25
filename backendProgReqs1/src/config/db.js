aadd071b886bf694e2b4e785035d86a0d0cc7be3
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-11-24 19:19:43 -0500

    actualizar bd de postgresql para despliegue demo

--- backendProgReqs1/src/config/db.js
+++ backendProgReqs1/src/config/db.js
@@ -5,7 +5,12 @@ const {db} = require('./config')
 const sequelize = new Sequelize(db.database, db.user, db.password, { 
     host: db.host,
     dialect: db.dialect,  
-    
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // You may need to set this to true based on your server's SSL configuration
+        },
+      },
 })
 
 module.exports = sequelize
