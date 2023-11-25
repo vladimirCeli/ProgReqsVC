67bbfaa187e7c3485a2408434d976f1f751a6a9b
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-11-24 21:40:07 -0500

    cambios en allowcorsv0.3

--- backendProgReqs1/src/index.js
+++ backendProgReqs1/src/index.js
@@ -41,6 +41,18 @@ require('./config/mongodb')
 
 app.use(credentials)
 app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    next();
+})
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
