d80654ff95208ff3f9fdc73e2e04a382283b5c6b
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-11-27 18:16:12 -0500

    cambios en cors para permisos en vista previa producción

--- backendProgReqs1/src/config/allowedOrigins.js
+++ backendProgReqs1/src/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:4000',
     'http://localhost:3001',
     'http://localhost:5173',
+    'http://localhost:4173',
     'https://progreqsvc.onrender.com',
     'https://progreqs.onrender.com',
     'https://progresqscic.onrender.com',
